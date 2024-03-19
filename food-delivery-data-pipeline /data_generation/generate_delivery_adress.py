5bf752245453078e2b92139849397d2473a04a2b
Diego VillatoroMx <107954399+diegovillatoromx@users.noreply.github.com>
2024-03-19 13:54:24 -0600

    Add files via upload

---
+++ food-delivery-data-pipeline /data_generation/generate_delivery_adress.py
@@ -0,0 +1,20 @@
+import json
+import random
+
+def generate_delivery_addresses():
+    with open('address_data.json') as f:
+        data = json.load(f)
+    
+    street_names = data['street_names']
+    neighborhoods = data['neighborhoods']
+    city = data['city']
+    state = data['state']
+
+    street_numbers = [random.randint(100, 999) for _ in range(len(street_names))]
+    
+    delivery_addresses = []
+    for i in range(len(street_names)):
+        address = f"{street_names[i]} #{street_numbers[i]}, {neighborhoods[i]}, {city}, {state}"
+        delivery_addresses.append(address)
+
+    return random.choice(delivery_addresses)
---
+++ food-delivery-data-pipeline /data_generation/generate_restaurant_adress.py
@@ -0,0 +1,20 @@
+import json
+import random
+
+def load_restaurant_data('restaurant_data.json'):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    return data
+
+def get_random_restaurant(restaurants):
+    # Choose a random restaurant category
+    category = random.choice(list(restaurants.keys()))
+    
+    # Choose a random restaurant from the selected category
+    restaurant = random.choice(restaurants[category])
+    
+    return {
+        "Category": category,
+        "Name": restaurant["Name"],
+        "Address": restaurant["Address"]
+    }
